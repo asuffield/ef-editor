42cdbf7969e6adf0bbe5c4bb7b52be222ed90f62
Andrew Suffield <asuffield@suffields.me.uk>
2012-04-09 23:31:16 +0100

    Add py2exe support

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+import py2exe
+
+setup(
+    version = "0.1.0",
+    description = "Image editor for eventsforce",
+    name = "ef-image-editor",
+
+    options = {
+        'py2exe': {
+            'includes': ['sip', 'PyQt4.QtSql'],
+            'dll_excludes': ['MSVCP90.dll'],
+            },
+        },
+
+    data_files = [
+        ('sqldrivers', [r'c:\Python27\Lib\site-packages\PyQt4\plugins\sqldrivers\qsqlite4.dll']),
+        ('imageformats', [r'c:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qjpeg4.dll',
+                          r'c:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qgif4.dll',
+                          r'c:\Python27\Lib\site-packages\PyQt4\plugins\imageformats\qmng4.dll',
+                          ]),
+        ],
+
+    # targets to build
+    windows = ["ef-image-editor.py"],
+    )
